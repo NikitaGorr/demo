847e41e6d08cad116e0f9c1668d206c572f8f051
NikitaGorr <nikitagorak@yandex.ru>
2021-06-26 18:23:31 +0300

    Изменение н1

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ package unknown.demo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("X_x");
+        int x;
+        x = 15;
+        System.out.println("x = " + x);
     }
 }
